5a8091a0f300d7344f44e72c937c9353942faf83
Deepakupreti2002 <deepakupreti875@gmail.com>
2024-07-19 18:43:28 +0530

    edit

--- Megavlog/src/components/index.js
+++ Megavlog/src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import logoutBtn from "./Header/logoutBtn";
-
+import Button from "./Button";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    logoutBtn
+    logoutBtn,
+    Button,
+    Input,
+    
 }

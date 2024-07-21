29346ab9dff3dda83bc3b85d53814ad078b97ab7
Deepakupreti2002 <deepakupreti875@gmail.com>
2024-07-21 15:52:09 +0530

    last Update

--- Megavlog/src/components/pages/AllPosts.jsx
+++ Megavlog/src/components/pages/AllPosts.jsx
@@ -1,23 +1,25 @@
-import React, {  useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import {Container, PostCard} from '../index'
 import service from '../../appwrite/config'
 
 function AllPosts() {
   const [posts,setPosts] = useState([])
-  
-  service.getPosts([]).then((posts)=>{
-    if (posts) {
-      setPosts(posts.documents)
-      console.log(posts);
-    }
-  })
+  useEffect(()=>{
+    service.getPosts([]).then((posts)=>{
+      if (posts) {
+        setPosts(posts.documents)
+        console.log(posts);
+      }
+    })
+  },[])
+ 
   return (
     <div className='w-full py-8'>
       <Container>
         <div className='flex flex-wrap'>
           {posts.map((post)=>(
             <div key={post.$id} className='p-2 w-1/4'>
-              <PostCard post={post}/> 
+              <PostCard {...post}/> 
             </div>
           ))}
 
